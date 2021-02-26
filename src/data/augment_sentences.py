0f8d4ea1ca42cb491e3f3a3a0c4c41ce30f00ada
williamNordAxon <william.rosengren@nordaxon.com>
2021-02-26 08:50:02 +0100

    Added a method to add a path for the destionation of the augmented file if it does not exist

--- src/data/augment_sentences.py
+++ src/data/augment_sentences.py
@@ -8,8 +8,9 @@ with the exception that the data are called source and target.
 import pandas as pd
 
 import nltk
+
 from pathlib import Path
-import click
+
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
@@ -18,35 +19,47 @@ t = Wordnet()
 
 # TODO: Fix the paths.
 
-def augment_text(text):
+def augment_text(text, nbr_of_iterations=1):
     """
     Augments an entire text.
-    :param text: The text that should be augmented.
+    :param text: The text that should be augmented. In the case of Freja, this is either one instance of a source
+                  or a target text.
+    :param nbr_of_iterations: The number of iterations to run the data augmentation for a given text.
     :return:
     """
     augmented_texts = []
-    #Make several different augmentations.
-    for k in range(5):
+    #Make several different augmentations for the same text.
+    for k in range(nbr_of_iterations):
         augmented_texts.append(t.augment(text))
 
     return augmented_texts
 
 
 def main(input_path: Path, output_path: Path):
-    #
+    """
+    The main method for augmenting data using
+    :param input_path:
+    :param output_path:
+    :return:
+    """
+
+    # Check if the path exists.
+    p = Path(output_path)
+    # If not, create the path.
+    if not p.exists():
+        Path(output_path).mkdir(parents=True, exist_ok=True)
 
     data_frame = pd.read_csv(input_path)
 
-    # Go through all the text in the
+    # Go through all the text in the csv file.
 
     M = data_frame.shape
 
     output_dict = {}
     id = 0
     for k in range(M[0]):
-        # Extract
+        # Extract source and targets texts. 
         src = data_frame.iloc[k]["src"]
-        # find_special_characters(src)
 
         target = data_frame.iloc[k]["target"]
 
@@ -58,18 +71,11 @@ def main(input_path: Path, output_path: Path):
             for k2 in range(len(target_aug)):
                 output_dict[id] = {"src": src_aug[k1], "target": target_aug[k2]}
                 id += 1
-                #output_text += "source: {0}\ntarget: {1}\nepisode_done \n \n".format(src_aug[k1], target_aug[k2])
 
-
-    #save_path = "C:\\Users\\WilliamRosengren\\Documents\\Jobb\\Freja\\freja\\data\\augmented\\"
     src_target_df = pd.DataFrame.from_dict(output_dict, orient='index')
     print("Storing data at: {0}".format(output_dict))
     src_target_df.to_csv(output_path + "augmented_data.csv", index_label='Name')
 
-
-    #file1 = open(save_path + "augmented_data.txt", "w")
-    #file1.write(output_text)
-    #file1.close()
 
 #"""
 if __name__ == "__main__":
