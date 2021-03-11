de163ee7080eaf2a3932c932e9de9b4cea60d87b
bornhagel <alexander.hagelborn@nordaxon.com>
2021-03-11 12:33:54 +0100

    fix: parallel fika and intervju

--- src/api/api_main.py
+++ src/api/api_main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from argparse import Namespace
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from typing import Optional
 
 
 class Message(BaseModel):
@@ -14,9 +15,10 @@ class Message(BaseModel):
 
 class InitChat(BaseModel):
     # Defines body for initial contact where a user starts a chat with Emely
+    persona: str
     conversation_id: int
     name: str
-    job: str
+    job: Optional[str] = None
 
 
 class BaseResponse(BaseModel):
@@ -35,10 +37,11 @@ class SetPersona(BaseModel):
 
 
 brain = FastAPI()
+conversation_id_to_persona = {}
 
-interview_persona = Namespace(model_name='blenderbot_small-90M@8', local_model=True,
+interview_persona = Namespace(model_name='blenderbot_small-90M', local_model=True,
                               chat_mode='interview')
-fika_persona = Namespace(model_name='blenderbot-400M-distill', local_model=True,
+fika_persona = Namespace(model_name='blenderbot_small-90M', local_model=True,
                          chat_mode='chat')
 
 interview_world = InterviewWorld(**vars(interview_persona))
@@ -48,9 +51,9 @@ world = None
 
 # Models aren't loaded
 async def init_config():
-    global interview_world, fika_world, world
-    world = interview_world  # Automatically choose this?
-    world.load_model()
+    global interview_world, fika_world
+    interview_world.load_model()
+    fika_world.load_model()
     return
 
 
@@ -60,6 +63,15 @@ brain.add_event_handler("startup", init_config)
 @brain.post('/message')
 def chat(msg: Message, response: Response):
     conversation_id, message = msg.conversation_id, msg.message
+    persona = conversation_id_to_persona[conversation_id]
+    if persona == 'fika':
+        world = fika_world
+    elif persona == 'intervju':
+        world = interview_world
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return response
+
     try:
         episode_done = world.observe(message, conversation_id)
         reply = world.act(conversation_id)
@@ -67,8 +79,7 @@ def chat(msg: Message, response: Response):
     except KeyError:
         response.status_code = status.HTTP_404_NOT_FOUND
         episode_done = True
-        reply = ''
-        return response
+        reply = 'conversation_id not found'
     response = BaseResponse(reply=reply, episode_done=episode_done)
     brain_response = BrainResponse(response=response)
     return brain_response
@@ -76,6 +87,19 @@ def chat(msg: Message, response: Response):
 
 @brain.post('/init', status_code=201)
 def new_chat(msg: InitChat, response: Response):
+    persona = msg.persona
+    if persona == 'fika':
+        world = fika_world
+    elif persona == 'intervju':
+        if msg.job is None:
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return response
+        else:
+            world = interview_world
+    else:
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return response
+    conversation_id_to_persona[msg.conversation_id] = persona
     greeting = world.init_conversation(**msg.dict())
     response = {'reply': greeting,
                 'episode_done': False
@@ -85,44 +109,44 @@ def new_chat(msg: InitChat, response: Response):
     brain_response = BrainResponse(**brain_response)
     return brain_response
 
-
-@brain.get('/init', status_code=200)
-def get_chats():
-    json_compatible_item_data = jsonable_encoder(list(world.dialogues.keys()))
-    return JSONResponse(content=json_compatible_item_data)
-
-
-@brain.get('/persona', status_code=200)
-def get_persona():
-    persona = type(world).__name__
-    response = {'reply': 'Current world is {}'.format(persona),
-                'episode_done': True}
-    response = BaseResponse(**response)
-    return response
-
-
-@brain.put('/persona')
-def set_persona(msg: SetPersona, response: Response):
-    global world, fika_world, interview_world
-    if msg.new_persona == 'intervju' or msg.new_persona == 'interview':
-        if 'Interview' in type(world).__name__:
-            # Interview new_persona already running: HTTP already reported
-            response.status_code = 208
-        else:
-            world.unload_model()
-            world = interview_world
-            world.load_model()
-            response.status_code = status.HTTP_201_CREATED
-
-    elif msg.new_persona == 'fika':
-        if 'Chat' in type(world).__name__:
-            response.status_code = 208
-        else:
-            world.unload_model()
-            world = fika_world
-            world.load_model()
-            response.status_code = status.HTTP_201_CREATED
-    else:
-        response.status_code = status.HTTP_404_NOT_FOUND
-
-    return response
+#
+# @brain.get('/init', status_code=200)
+# def get_chats():
+#     json_compatible_item_data = jsonable_encoder(list(world.dialogues.keys()))
+#     return JSONResponse(content=json_compatible_item_data)
+#
+#
+# @brain.get('/persona', status_code=200)
+# def get_persona():
+#     persona = type(world).__name__
+#     response = {'reply': 'Current world is {}'.format(persona),
+#                 'episode_done': True}
+#     response = BaseResponse(**response)
+#     return response
+#
+#
+# @brain.put('/persona')
+# def set_persona(msg: SetPersona, response: Response):
+#     global world, fika_world, interview_world
+#     if msg.new_persona == 'intervju' or msg.new_persona == 'interview':
+#         if 'Interview' in type(world).__name__:
+#             # Interview new_persona already running: HTTP already reported
+#             response.status_code = 208
+#         else:
+#             world.unload_model()
+#             world = interview_world
+#             world.load_model()
+#             response.status_code = status.HTTP_201_CREATED
+#
+#     elif msg.new_persona == 'fika':
+#         if 'Chat' in type(world).__name__:
+#             response.status_code = 208
+#         else:
+#             world.unload_model()
+#             world = fika_world
+#             world.load_model()
+#             response.status_code = status.HTTP_201_CREATED
+#     else:
+#         response.status_code = status.HTTP_404_NOT_FOUND
+#
+#     return response
