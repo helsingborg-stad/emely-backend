17196204d6ea8b90b7dc2e9d5bf89b077cbd4321
bornhagel <alexander.hagelborn@nordaxon.com>
2021-04-07 16:06:05 +0200

    fix: tweaked request bodys

--- src/api/bodys.py
+++ src/api/bodys.py
@@ -13,43 +13,6 @@ class LanguageEnum(str, Enum):
     en = 'en'
 
 
-class InitBody(BaseModel):
-    # Defines body for initial contact where a user starts a chat with Emely
-    name: str
-    job: str
-    created_at: str
-    persona: PersonaEnum
-    development_testing: bool
-    webapp_local: bool
-    webapp_url: str
-    webapp_version: str
-    webapp_git_build: str
-    brain_local: bool
-    brain_url: str
-    lang: LanguageEnum
-    password: str
-    user_ip_number: str
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "name": "Wilma",
-                "job": "snickare",
-                "persona": "intervju",                
-                "development_testing": True,
-                "webapp_local": True,
-                "webapp_url": "swaggerdocs",
-                "webapp_version": "NA",
-                "webapp_git_build": "NA",
-                "brain_local": is_gcp_instance(),
-                "brain_url": "NA",
-                "lang": "sv",
-                "password": "KYgZfDG6P34H56WJM996CKKcNG4",
-                "user_ip_number": "127.0.0.1",
-            }
-        }
-
-
 class UserMessage(BaseModel):
     conversation_id: str
     response_time: float
@@ -69,6 +32,42 @@ class UserMessage(BaseModel):
                 "recording_used": False,
                 "lang": "sv",
                 "password": "KYgZfDG6P34H56WJM996CKKcNG4",
+            }
+        }
+
+
+class InitBody(BaseModel):
+    # Defines body for initial contact where a user starts a chat with Emely
+    name: str
+    job: str
+    created_at: str
+    persona: PersonaEnum
+    development_testing: bool
+    webapp_local: bool
+    webapp_url: str
+    webapp_version: str
+    webapp_git_build: str
+    brain_url: str
+    lang: LanguageEnum
+    password: str
+    user_ip_number: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Wilma",
+                "job": "snickare",
+                "created_at": 'testtime',
+                "persona": "intervju",
+                "development_testing": True,
+                "webapp_local": True,
+                "webapp_url": "swaggerdocs",
+                "webapp_version": "NA",
+                "webapp_git_build": "NA",
+                "brain_url": "NA",
+                "lang": "sv",
+                "password": "KYgZfDG6P34H56WJM996CKKcNG4",
+                "user_ip_number": "127.0.0.1",
             }
         }
 
