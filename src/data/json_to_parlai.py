1a75accf121b4a8c387819d26a94f4b5287b395d
williamNordAxon <william.rosengren@nordaxon.com>
2021-04-26 14:52:41 +0200

    Refactor: Updated to the lastest version of json_to_parlai.py

--- src/data/json_to_parlai.py
+++ src/data/json_to_parlai.py
@@ -1,7 +1,7 @@
 """
 Transforms data from the .json format to a .txt-file that can be used when training Parlai.
 """
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 import json
 import random
@@ -10,18 +10,26 @@ from pathlib import Path
 import warnings
 
 
-def get_first_data():
+def get_first_data(job=None):
     """Returns the first data to each interaction as a string."""
     # TODO: Add better examples than just an empty string. For example have a list
     # TODO: of opening lines and sample from these.
     # TODO: format with position
 
-    input_list = ["Thanks for having me!", \
-                  "I'm excited about this interview.", \
-                  "I'm a little nervous but we can start the interview now.",
-                  " "]
-    i = random.randint(0, len(input_list) - 1)
-    return "text:{0}\t".format(input_list[i])  # Begin each new dialouge with an empty string as input.
+    if job is None:
+        input_list = ["Thanks for having me!", \
+                      "I'm excited about this interview.", \
+                      "I'm a little nervous but we can start the interview now.",
+                      " "]
+        i = random.randint(0, len(input_list) - 1)
+        message = input_list[i]
+    else:
+        input_list = ["I'm applying for the job as __job__",
+                      "I want to be a __job__",
+                      "I'm here for the interview for the position as __job__"]
+        i = random.randint(0, len(input_list) - 1)
+        message = input_list[i].replace('__job__', job.lower().strip('\n'))
+    return "text:{0}\t".format(message)
 
 
 def open_json(path):
@@ -40,24 +48,42 @@ def extract_data(data, args):
     #  Different ways of creating the beginning of the parlai formatted data
     if args.no_hardcoded_question:
         output = ''
+    elif data['emely_start'] and args.add_position_context:
+        output = get_first_data(job=data['position'])
+    elif args.hardcoded_question_as_context and data['emely_start']:
+        output = ''
     elif data['emely_start']:
         output = get_first_data()
     else:
         output = ''
 
     k = 1
-    # Go through all the dialouge string.
+    # Go through all the dialogue string.
     # Skip the first entry as this is one of the basic questions asked by Emely.
-    for data_tup in data["dialouge"]:
+    for data_tup in data["dialogue"]:
         if args.no_hardcoded_question and k == 1:
             k += 1
             continue
+        elif args.hardcoded_question_as_context and data['emely_start']:
+            # We want the first part of the episode to be: text:<emelys hardcoded question>\n<user reply>
+            if k == 1:
+                hardcoded_question = data_tup[1].strip('\n').strip()
+                k += 1
+                continue
+            elif k == 2:
+                question_reply = data_tup[1].strip('\n').strip()
+                output = 'text:{}{}{}\t'.format(hardcoded_question, r'\n',
+                                                question_reply)  # We format with a literal \n here. Important!
+                k += 1  # We skip the two first iterations since we've done em here
+                continue
+            else:
+                pass
 
         u_or_e = data_tup[0]
         text = data_tup[1].replace("\n", "")  # Remove the line break.
         text = text.strip()  # Trim whitespaces
         # Check if the interaction contains XXX(literally XXX). This is a placeholder for the text
-        # and if it is contained in the text, the entire dialouge should be thrown out.
+        # and if it is contained in the text, the entire dialogue should be thrown out.
         if "XXX" in text:
             return "contained XXX", False
 
@@ -71,7 +97,7 @@ def extract_data(data, args):
                 # Remove the last line break.
 
                 warnings.warn("Last tag is text")
-                output = output.rsplit('\n', 1)[0] # Removes everything after the last tag
+                output = output.rsplit('\n', 1)[0]  # Removes everything after the last tag
                 output += "\tepisode_done:True\n"
         else:
             output += "{0}:{1}{2}".format(tag, text, end)
@@ -103,7 +129,8 @@ def main(input_path, output_filename, args):
             warning_string = "The data:\n {0}\n  {1}. Not adding data.".format(data, output)
             warnings.warn(warning_string)
             continue
-        output_string += output  # Append the output.data
+        else:
+            output_string += output  # Append the output.data
 
     f = open(store_path, "w")
     f.write(output_string)
@@ -116,15 +143,19 @@ if __name__ == "__main__":
     --input_path: The path to the input-directory where all the .json files are stored. 
     --output_filename: The path where the data should be be stored. 
     --no_hardcoded_question: Skips the first question in the dialogue
+    --add_position_context: Adds something before emelys question
+    --hardcoded_question_as_context: Formats the data similarly to blended_skill_talk where the the model gets two interactions as context
     """
-
 
     parser = ArgumentParser()
     parser.add_argument('--input_path', type=str, required=True)
     parser.add_argument('--output_filename', type=str, required=True)
     parser.add_argument('--no_hardcoded_question', action='store_true', required=False,
                         help='Skips hardcoded question in parlai data')
-    parser.set_defaults(no_hardcoded_question=False)
+    parser.add_argument('--add_position_context', action='store_true', required=False)
+    parser.add_argument('--hardcoded_question_as_context', action='store_true', required=False)
+    parser.set_defaults(no_hardcoded_question=False,
+                        add_position_context=False,
+                        hardcoded_question_as_context=True)
     args = parser.parse_args()
     main(args.input_path, args.output_filename, args)
-
