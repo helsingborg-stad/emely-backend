b0616e2038d40c3fb6a0a5cf4450c33769725add
bornhagel <alexander.hagelborn@nordaxon.com>
2021-04-28 15:16:03 +0200

    change: datasplit is 95/5/0. Beware: test set is identical to valid set

--- src/data/create_parlai_datasets.py
+++ src/data/create_parlai_datasets.py
@@ -34,9 +34,13 @@ def episode_to_parlai(episodes):
 
 
 def create_datasets(episodes):
-    """ Splits data set twice into 90%, 9%, 1% sets used for train, validation, test respectively"""
-    train_episodes, other_episodes = train_test_split(episodes, train_size=0.9, test_size=0.1)
-    val_episodes, test_episodes = train_test_split(other_episodes, train_size=0.9, test_size=0.1)
+    """ Splits data set twice into 95%, 5%, 0% sets used for train, validation, test respectively
+        - Due to low amounts of data we set the test and valid sets to be equal!!!
+    """
+    train_episodes, other_episodes = train_test_split(episodes, train_size=0.95, test_size=0.05)
+    #val_episodes, test_episodes = train_test_split(other_episodes, train_size=0.9, test_size=0.1)
+    val_episodes = other_episodes
+    test_episodes = other_episodes
 
     train_text = episode_to_parlai(train_episodes)
     val_text = episode_to_parlai(val_episodes)
