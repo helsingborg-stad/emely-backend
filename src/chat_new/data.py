6d99cdcff63f72d27491d24dfc70d698ace2c3fc
bornhagel <alexander.hagelborn@nordaxon.com>
2021-10-19 17:11:17 +0200

    dataclasses -> data to not collide with dataclasses module

--- src/chat_new/data.py
+++ src/chat_new/data.py
@@ -4,28 +4,47 @@ from dataclasses import dataclass, asdict, field
 
 
 @dataclass
+class ConversationInit:
+    brain_url: str
+    created_at: str
+    development_testing: bool
+    lang: str
+    name: str
+    persona: str
+    user_ip_number: str
+    webapp_local: bool
+    webapp_url: str
+    webapp_version: str
+
+    job: str = None
+    has_experience: bool = True
+    enable_small_talk: bool = True
+    user_id: str = None
+
+
+@dataclass
 class Conversation:
     brain_url: str
     created_at: str
     current_dialog_block: str
     development_testing: bool
     episode_done: bool
+    job: str
     lang: str
     name: str
     persona: str
     question_list: List[Dict]
+    user_id: str
     user_ip_number: str
     webapp_local: bool
     webapp_url: str
     webapp_version: str
 
     # Default values
-    conversation_id: str = None  # Tricky one... Is set first when we've pushed to firestore so it has to be None at initialisation
+    conversation_id: str = None  # Is set first when we've pushed to firestore so it has to be None at initialisation
     current_dialog_block_length: str = 0
-    job: str = None
     messages: List = []
     nbr_messages: int = 0
-    user_id: str = None
 
     def add_message(self, message: UserMessage, who, message_en, etc):
         # TODO: What info is needed to convert UserMessage to FirestoreMessage?
@@ -60,13 +79,14 @@ class UserMessage:
 
 
 @dataclass
-class FirestoreMessage(UserMessage):
+class Message(UserMessage):
     "Extends Usermessage to include more data saved in database"
     conversation_id: str
     lang: str
     message_nbr: int
     message: str
     message_en: str
+    progress: float
     response_time: float
     who: str
 
