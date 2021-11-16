36db754d93e149ddedca4d33b278e306798517b6
bornhagel <alexander.hagelborn@nordaxon.com>
2021-11-16 16:03:42 +0100

    fix: bug (if USE_HUGGIN...)

--- chat/dialog/worlds.py
+++ chat/dialog/worlds.py
@@ -70,7 +70,7 @@ class DialogWorld:
         """
         self.rasa_model.wake_up()
         self.interview_flow_handler.interview_model.wake_up()
-        if os.environ["USE_HUGGINGFACE_FIKA"]:
+        if os.environ["USE_HUGGINGFACE_FIKA"] == '1':
             self.interview_flow_handler.huggingface_fika_model.wake_up()
         self.interview_flow_handler.fika_model.wake_up()
         return
