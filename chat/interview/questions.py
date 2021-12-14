4aaebbe7b8a8be2bc8b4de23bf786ac5713ff707
Christoffer Kjellson <c.kjellson@gmail.com>
2021-12-14 13:17:06 +0100

    fix: loads questions for general interview correctly

--- chat/interview/questions.py
+++ chat/interview/questions.py
@@ -105,12 +105,14 @@ class QuestionGenerator:
                 self.candidate_questions.loc[:, "fit_4_no_exp"] == True
             ]
 
-        # If job does not exist sample random questions
-        if job not in set(self.question_df.loc[:, "job"]) or job == "Allmän":
+        # If job does not exist assume "Allmän intervjuträning"
+        if job not in set(self.question_df.loc[:, "job"]) or job == "Allmän intervjuträning":
+            self.candidate_questions = self.candidate_questions[self.candidate_questions.loc[:, "fit_4_general"] == 1]
             self.temp_config["nbr_random_questions"] += self.temp_config[
                 "nbr_job_questions"
-            ]
+            ] + 1
             self.temp_config["nbr_job_questions"] = 0
+            self.temp_config["nbr_always_questions"] -= 1   # In this case we currently only have one "always"-question
 
         # We only need to return a list of question strings
         question_list = []
