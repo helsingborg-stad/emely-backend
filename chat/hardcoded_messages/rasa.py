fafb5b3170b4b2bd5c6fdd0963613cd3499e5f6c
bornhagel <alexander.hagelborn@nordaxon.com>
2021-12-15 18:22:06 +0100

    feat: rasa monitoring also in fika + rasa_act for rasa.fika_intents

--- chat/hardcoded_messages/rasa.py
+++ chat/hardcoded_messages/rasa.py
@@ -5,6 +5,8 @@ replies = {
     "dont_understand": "Förlåt, ibland säger jag saker som är lite konstiga. ",  # This is only used in backup cases
 }
 
+fika_intents = ["tell_sfi"]
+
 # Used in case user didn't understand something Emely said that was hardcoded
 dont_understand_transition = "Okej, då går vi vidare till nästa fråga."
 dont_understand_other_formulation = "Jag formulerar om frågan."
